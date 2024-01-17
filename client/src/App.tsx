25eeb2d924b92c05877bbfd2e3de6ad4e39a1387
izoo0 <isaiahwainaina34@gmail.com>
2024-01-17 14:07:01 +0300

    added client view with react

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import PostList from './feature/post/PostList'
+
+function App() {
+
+  // Fetch Post from the API
+  return (
+    <>
+      <h1>React On Rails Blog</h1>
+      <h4>Find this layout on client/src/app.tsc</h4>
+      <PostList/>
+    </>
+  )
+}
+
+export default App
